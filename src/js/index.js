671bdfb40ffa623f22508923f6e5d2d5d05f1558
David Alvarez <marava.jr@gmail.com>
2021-12-31 18:46:43 +0100

    Comments added for data filter

--- src/js/index.js
+++ src/js/index.js
@@ -31,191 +31,191 @@ function changeformatDateJSON() {
     }
   }
 }
-  // ESTA FUNCIÓN CREA CADA TARJETA DE EVENTO
-  function createEvent(container, listEvents) {
-    for(let position in listEvents) {
-      //Llamar función que imprime la fecha en el orden deseado
-      let dateStart = dateFormat(listEvents[position].dateStart, true);
-      let containerCard = document.createElement("div");
-      containerCard.className = "container-card";
-      containerCard.dataset.id = listEvents[position].id
-      // AÑADE EL EVENTO A LA TARJETA
-      containerCard.addEventListener("click", dataModal);
-      //DIV DE LA IMAGEN
-      let photoEvent = document.createElement("div");
-      photoEvent.className = "photoEvent";
-      //BOTON FAVORITOS
-      let bookmarkContainer = document.createElement("div");
-      bookmarkContainer.className = "bookmark"
-      let bookmark = document.createElement("img");
-      bookmark.src = listEvents[position].bookmark ? "./img/icons/bookmark-selected.svg"  : "./img/icons/bookmark.svg";
-      bookmark.dataset.name = listEvents[position].id
-      // bookmark.removeEventListener("click",dataModal)
-      bookmark.addEventListener('click',selectedBookmark)
-      //IMAGEN
-      let image = document.createElement("img");
-      image.src = listEvents[position].photoEvent;
-      //DATOS TARJETA
-      let infoCard = document.createElement("div");
-      infoCard.className = "info-card";
-      // NOMBRE
-      let name = document.createElement("h3");
-      name.innerText = listEvents[position].nameEvent;
-      // LUGAR
-      let place = document.createElement("p");
-      place.innerText = listEvents[position].cityLocation;
-      // BARRA DE ICONOS
-      let bar = document.createElement("div");
-      bar.className = "icons-bar";
-      // FECHA
-      let date = document.createElement("p");
-      date.innerText = `Solo el ${dateStart}`;
-      if(listEvents[position].hasOwnProperty("dateFinal")){
-        let dateF = dateFormat(listEvents[position].dateFinal,true );
-        date.innerText = `Desde el ${dateStart}  al ${dateF}`;
-      }
-      container.appendChild(containerCard);
-      containerCard.appendChild(photoEvent);
-      photoEvent.appendChild(bookmarkContainer);
-      bookmarkContainer.appendChild(bookmark);
-      photoEvent.appendChild(image);
-      containerCard.appendChild(infoCard);
-      infoCard.appendChild(name);
-      infoCard.appendChild(place);
-      infoCard.appendChild(date);
-      infoCard.appendChild(bar);
+// ESTA FUNCIÓN CREA CADA TARJETA DE EVENTO
+function createEvent(container, listEvents) {
+  for(let position in listEvents) {
+    //Llamar función que imprime la fecha en el orden deseado
+    let dateStart = dateFormat(listEvents[position].dateStart, true);
+    let containerCard = document.createElement("div");
+    containerCard.className = "container-card";
+    containerCard.dataset.id = listEvents[position].id
+    // AÑADE EL EVENTO A LA TARJETA
+    containerCard.addEventListener("click", dataModal);
+    //DIV DE LA IMAGEN
+    let photoEvent = document.createElement("div");
+    photoEvent.className = "photoEvent";
+    //BOTON FAVORITOS
+    let bookmarkContainer = document.createElement("div");
+    bookmarkContainer.className = "bookmark"
+    let bookmark = document.createElement("img");
+    bookmark.src = listEvents[position].bookmark ? "./img/icons/bookmark-selected.svg"  : "./img/icons/bookmark.svg";
+    bookmark.dataset.name = listEvents[position].id
+    // bookmark.removeEventListener("click",dataModal)
+    bookmark.addEventListener('click',selectedBookmark)
+    //IMAGEN
+    let image = document.createElement("img");
+    image.src = listEvents[position].photoEvent;
+    //DATOS TARJETA
+    let infoCard = document.createElement("div");
+    infoCard.className = "info-card";
+    // NOMBRE
+    let name = document.createElement("h3");
+    name.innerText = listEvents[position].nameEvent;
+    // LUGAR
+    let place = document.createElement("p");
+    place.innerText = listEvents[position].cityLocation;
+    // BARRA DE ICONOS
+    let bar = document.createElement("div");
+    bar.className = "icons-bar";
+    // FECHA
+    let date = document.createElement("p");
+    date.innerText = `Solo el ${dateStart}`;
+    if(listEvents[position].hasOwnProperty("dateFinal")){
+      let dateF = dateFormat(listEvents[position].dateFinal,true );
+      date.innerText = `Desde el ${dateStart}  al ${dateF}`;
+    }
+    container.appendChild(containerCard);
+    containerCard.appendChild(photoEvent);
+    photoEvent.appendChild(bookmarkContainer);
+    bookmarkContainer.appendChild(bookmark);
+    photoEvent.appendChild(image);
+    containerCard.appendChild(infoCard);
+    infoCard.appendChild(name);
+    infoCard.appendChild(place);
+    infoCard.appendChild(date);
+    infoCard.appendChild(bar);
 
 
-      // ICONO GRATUITO / DE PAGO
-      let freeIconContainer = document.createElement("div");
-      freeIconContainer.className = "tooltip";
-      let freeIcon = document.createElement("img");
-      let freeIconText = document.createElement("span");
-      freeIconText.className = "tooltip-text";
-      photoEvent.appendChild(freeIconContainer);
-      freeIconContainer.appendChild(freeIcon);
-      freeIconContainer.appendChild(freeIconText);
+    // ICONO GRATUITO / DE PAGO
+    let freeIconContainer = document.createElement("div");
+    freeIconContainer.className = "tooltip";
+    let freeIcon = document.createElement("img");
+    let freeIconText = document.createElement("span");
+    freeIconText.className = "tooltip-text";
+    photoEvent.appendChild(freeIconContainer);
+    freeIconContainer.appendChild(freeIcon);
+    freeIconContainer.appendChild(freeIconText);
 
-      if (listEvents[position].free) {
-        freeIconText.textContent = "Evento GRATUITO";
-        freeIcon.src = "./img/icons/gratis.svg";
-        freeIcon.alt = "Evento GRATUITO";
-      } else {
-        freeIconText.textContent = "Evento de PAGO";
-        freeIcon.src = "./img/icons/Pago-euro.svg";
-        freeIcon.alt = "Evento de PAGO";
-      }
-      // ICONO BENÉFICO
-      if(listEvents[position].charity) {
-        let charityIconContainer = document.createElement("div");
-        let charityIcon = document.createElement("img");
-        let charityIconText = document.createElement("p");
-        charityIconText.textContent = "Benéfico";
-        charityIcon.src = "./img/icons/Charity.svg";
-        bar.appendChild(charityIconContainer);
-        charityIconContainer.appendChild(charityIcon);
-        charityIconContainer.appendChild(charityIconText);
-      }
-      // ICONOS DE CATEGORÍAS
-      for(let cat in listEvents[position].category) {
-        switch(listEvents[position].category[cat]) {
-          case "Christmas":
-            let xmasIconContainer = document.createElement("div");
-            let xmasIcon = document.createElement("img");
-            let xmasIconText = document.createElement("p");
-            xmasIconText.textContent = "Navidad";
-            xmasIcon.src = "./img/icons/Navidad.svg";
-            bar.appendChild(xmasIconContainer);
-            xmasIconContainer.appendChild(xmasIcon);
-            xmasIconContainer.appendChild(xmasIconText);
-            break;
-          case "Kids":
-            let kidsIconContainer = document.createElement("div");
-            let kidsIcon = document.createElement("img");
-            let kidsIconText = document.createElement("p");
-            kidsIconText.textContent = "Infantil";
-            kidsIcon.src = "./img/icons/Kids.svg";
-            bar.appendChild(kidsIconContainer);
-            kidsIconContainer.appendChild(kidsIcon);
-            kidsIconContainer.appendChild(kidsIconText);
-            break;
-          case "Play":
-            let playIconContainer = document.createElement("div");
-            let playIcon = document.createElement("img");
-            let playIconText = document.createElement("p");
-            playIconText.textContent = "Lúdico";
-            playIcon.src = "./img/icons/Play.svg";
-            bar.appendChild(playIconContainer);
-            playIconContainer.appendChild(playIcon);
-            playIconContainer.appendChild(playIconText);
-            break;
-          case "Music":
-            let musicIconContainer = document.createElement("div");
-            let musicIcon = document.createElement("img");
-            let musicIconText = document.createElement("p");
-            musicIconText.textContent = "Música";
-            musicIcon.src = "./img/icons/Music.svg";
-            bar.appendChild(musicIconContainer);
-            musicIconContainer.appendChild(musicIcon);
-            musicIconContainer.appendChild(musicIconText);
-            break;
-          case "Sports":
-            let sportIconContainer = document.createElement("div");
-            let sportIcon = document.createElement("img");
-            let sportIconText = document.createElement("p");
-            sportIconText.textContent = "Deporte";
-            sportIcon.src = "./img/icons/Sports.svg";
-            bar.appendChild(sportIconContainer);
-            sportIconContainer.appendChild(sportIcon);
-            sportIconContainer.appendChild(sportIconText);
-            break;
-          case "Theatre":
-            let theatreIconContainer = document.createElement("div");
-            let theatreIcon = document.createElement("img");
-            let theatreIconText = document.createElement("p");
-            theatreIconText.textContent = "Teatro";
-            theatreIcon.src = "./img/icons/Theatre.svg";
-            bar.appendChild(theatreIconContainer);
-            theatreIconContainer.appendChild(theatreIcon);
-            theatreIconContainer.appendChild(theatreIconText);
-            break;
-          case "Party":
-            let partyIconContainer = document.createElement("div");
-            let partyIcon = document.createElement("img");
-            let partyIconText = document.createElement("p");
-            partyIconText.textContent = "Fiestas";
-            partyIcon.src = "./img/icons/Cocktail.svg";
-            bar.appendChild(partyIconContainer);
-            partyIconContainer.appendChild(partyIcon);
-            partyIconContainer.appendChild(partyIconText);
-            break;
-          case "Food":
-            let foodIconContainer = document.createElement("div");
-            let foodIcon = document.createElement("img");
-            let foodIconText = document.createElement("p");
-            foodIconText.textContent = "Gastronómico";
-            foodIcon.src = "./img/icons/Food.svg";
-            bar.appendChild(foodIconContainer);
-            foodIconContainer.appendChild(foodIcon);
-            foodIconContainer.appendChild(foodIconText);
-            break;
-          case "Museum":
-            let museumIconContainer = document.createElement("div");
-            let museumIcon = document.createElement("img");
-            let museumIconText = document.createElement("p");
-            museumIconText.textContent = "Museo";
-            museumIcon.src = "./img/icons/Museum.svg";
-            bar.appendChild(museumIconContainer);
-            museumIconContainer.appendChild(museumIcon);
-            museumIconContainer.appendChild(museumIconText);
-            break;
-          default:
-            console.error(`Hay ningun caso con ese nombre ${listEvents[position].category[cat]}`)
-            break;
-        }
+    if (listEvents[position].free) {
+      freeIconText.textContent = "Evento GRATUITO";
+      freeIcon.src = "./img/icons/gratis.svg";
+      freeIcon.alt = "Evento GRATUITO";
+    } else {
+      freeIconText.textContent = "Evento de PAGO";
+      freeIcon.src = "./img/icons/Pago-euro.svg";
+      freeIcon.alt = "Evento de PAGO";
+    }
+    // ICONO BENÉFICO
+    if(listEvents[position].charity) {
+      let charityIconContainer = document.createElement("div");
+      let charityIcon = document.createElement("img");
+      let charityIconText = document.createElement("p");
+      charityIconText.textContent = "Benéfico";
+      charityIcon.src = "./img/icons/Charity.svg";
+      bar.appendChild(charityIconContainer);
+      charityIconContainer.appendChild(charityIcon);
+      charityIconContainer.appendChild(charityIconText);
+    }
+    // ICONOS DE CATEGORÍAS
+    for(let cat in listEvents[position].category) {
+      switch(listEvents[position].category[cat]) {
+        case "Christmas":
+          let xmasIconContainer = document.createElement("div");
+          let xmasIcon = document.createElement("img");
+          let xmasIconText = document.createElement("p");
+          xmasIconText.textContent = "Navidad";
+          xmasIcon.src = "./img/icons/Navidad.svg";
+          bar.appendChild(xmasIconContainer);
+          xmasIconContainer.appendChild(xmasIcon);
+          xmasIconContainer.appendChild(xmasIconText);
+          break;
+        case "Kids":
+          let kidsIconContainer = document.createElement("div");
+          let kidsIcon = document.createElement("img");
+          let kidsIconText = document.createElement("p");
+          kidsIconText.textContent = "Infantil";
+          kidsIcon.src = "./img/icons/Kids.svg";
+          bar.appendChild(kidsIconContainer);
+          kidsIconContainer.appendChild(kidsIcon);
+          kidsIconContainer.appendChild(kidsIconText);
+          break;
+        case "Play":
+          let playIconContainer = document.createElement("div");
+          let playIcon = document.createElement("img");
+          let playIconText = document.createElement("p");
+          playIconText.textContent = "Lúdico";
+          playIcon.src = "./img/icons/Play.svg";
+          bar.appendChild(playIconContainer);
+          playIconContainer.appendChild(playIcon);
+          playIconContainer.appendChild(playIconText);
+          break;
+        case "Music":
+          let musicIconContainer = document.createElement("div");
+          let musicIcon = document.createElement("img");
+          let musicIconText = document.createElement("p");
+          musicIconText.textContent = "Música";
+          musicIcon.src = "./img/icons/Music.svg";
+          bar.appendChild(musicIconContainer);
+          musicIconContainer.appendChild(musicIcon);
+          musicIconContainer.appendChild(musicIconText);
+          break;
+        case "Sports":
+          let sportIconContainer = document.createElement("div");
+          let sportIcon = document.createElement("img");
+          let sportIconText = document.createElement("p");
+          sportIconText.textContent = "Deporte";
+          sportIcon.src = "./img/icons/Sports.svg";
+          bar.appendChild(sportIconContainer);
+          sportIconContainer.appendChild(sportIcon);
+          sportIconContainer.appendChild(sportIconText);
+          break;
+        case "Theatre":
+          let theatreIconContainer = document.createElement("div");
+          let theatreIcon = document.createElement("img");
+          let theatreIconText = document.createElement("p");
+          theatreIconText.textContent = "Teatro";
+          theatreIcon.src = "./img/icons/Theatre.svg";
+          bar.appendChild(theatreIconContainer);
+          theatreIconContainer.appendChild(theatreIcon);
+          theatreIconContainer.appendChild(theatreIconText);
+          break;
+        case "Party":
+          let partyIconContainer = document.createElement("div");
+          let partyIcon = document.createElement("img");
+          let partyIconText = document.createElement("p");
+          partyIconText.textContent = "Fiestas";
+          partyIcon.src = "./img/icons/Cocktail.svg";
+          bar.appendChild(partyIconContainer);
+          partyIconContainer.appendChild(partyIcon);
+          partyIconContainer.appendChild(partyIconText);
+          break;
+        case "Food":
+          let foodIconContainer = document.createElement("div");
+          let foodIcon = document.createElement("img");
+          let foodIconText = document.createElement("p");
+          foodIconText.textContent = "Gastronómico";
+          foodIcon.src = "./img/icons/Food.svg";
+          bar.appendChild(foodIconContainer);
+          foodIconContainer.appendChild(foodIcon);
+          foodIconContainer.appendChild(foodIconText);
+          break;
+        case "Museum":
+          let museumIconContainer = document.createElement("div");
+          let museumIcon = document.createElement("img");
+          let museumIconText = document.createElement("p");
+          museumIconText.textContent = "Museo";
+          museumIcon.src = "./img/icons/Museum.svg";
+          bar.appendChild(museumIconContainer);
+          museumIconContainer.appendChild(museumIcon);
+          museumIconContainer.appendChild(museumIconText);
+          break;
+        default:
+          console.error(`Hay ningun caso con ese nombre ${listEvents[position].category[cat]}`)
+          break;
       }
     }
   }
+}
 
 // ESTA FUNCIÓN CREA CADA VENTANA MODAL
 function dataModal(e) {
@@ -299,6 +299,7 @@ function createModal(id) {
     }
   });
 }
+
 // Función que convierte número del mes en nombre del mes reducido en español
 function dateFormat(month, dateShort = false) {
   const monthNames = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
@@ -308,6 +309,7 @@ function dateFormat(month, dateShort = false) {
   }
   return `${month.getDate()} ${monthFormat}`;
 }
+
 //Funciones para el botón de favoritos
 let arrayBookMark= [];
 //Functions for LocalStorage
@@ -339,7 +341,6 @@ function filterBookmarks() {
   }
   resetAndCreateEventsFiltered(listFilteredBookmark);
 }
-
 
 /* Función del slider de logos de patrocinadores
  * Selecciono todas las imágenes del contenedor con la variable Sponsors lo que me da un array
@@ -397,7 +398,6 @@ window.onscroll = () => {
 
 // función que muestra los eventos filtrados
 function resetAndCreateEventsFiltered(listFiltered) {
-  // currentListEvents = listFiltered;
   const resetContent = document.querySelector(".container");
   resetContent.innerHTML = "";
   if(listFiltered.length === [].length) {
@@ -419,7 +419,12 @@ btnEvent.addEventListener("click", (e) => {
     const listFilteredDate = currentListEvents.filter(event => (event.dateStart >= dateFrom && event.dateStart <= dateTo) ||
     (event.dateFinal >= dateFrom && event.dateFinal <= dateTo) ||
     (event.dateStart <= dateFrom && event.dateFinal >= dateTo));
-    // fechaInicio en Rango OR fechaFinal en Rango OR el evento dura más que el Rango
+    /*
+    * El evento:
+    * - Empieza en el rango
+    * - Termina en el rango
+    * - Dura más que el rango
+    */
     resetAndCreateEventsFiltered(listFilteredDate);
   }
 });
