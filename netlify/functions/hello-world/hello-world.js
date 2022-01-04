75c2b4606af524b7778be38d8ee0901ed3e1208b
TKadyear <tamarakadyear@gmail.com>
2022-01-04 11:53:34 +0100

    Trying differents parameters-Netlify functions

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,10 +1,14 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const summary = event.queryStringParameters.summary;
+    const location = event.queryStringParameters.location;
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({
+        message: `Titulo del evento: ${summary}
+      Localización ${location}`
+      }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
