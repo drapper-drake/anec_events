b86b831e3e5bd3faf960b0d0ff71296c8d0cc978
TKadyear <tamarakadyear@gmail.com>
2022-01-19 16:59:11 +0100

    Fixed cookies.html and privacy.html paths for vite compilation

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 const path = require("path");
+const { resolve } = require('path')
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 // const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/";   // GitHub
 const base = "/"; // Netlify
 
 module.exports = {
   root: "src",
-  base,
-  mode,
-  publicDir: "../public",
+  // nested: "src/nested",
+  base, mode, publicDir: "../public",
   build: {
     outDir: "../dist",
-    assetsDir: "./"
+    assetsDir: "./",
+    rollupOptions: {
+      input: {
+        main: resolve("src", 'index.html'),
+        cookies: resolve("src", 'cookies/cookies.html'),
+        privacy: resolve("src", 'privacy/privacy.html')
+      }
+
+    }
   }
-};
+}
