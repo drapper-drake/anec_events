44d15eb07614e9ef33c9e85a29ad6d48cd3b74a8
Lucía Ruiz <luciaruiz9722@gmail.com>
2022-02-02 12:51:52 +0100

    Added icons chevron left and right.

---
+++ src/components/ChevronLeft.vue
@@ -0,0 +1,8 @@
+<template>
+<a href="#"><img src="/img/icons/chevron-left.svg" alt="Página anterior"></a>
+
+</template>
+<script>
+
+</script>
+<style></style>
---
+++ src/components/ChevronRigth.vue
@@ -0,0 +1,8 @@
+<template>
+<a href="#"><img src="/img/icons/chevron-right.svg" alt="Página siguiente"></a>
+
+</template>
+<script>
+
+</script>
+<style></style>
