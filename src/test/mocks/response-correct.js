0c557af975d96c9a41fbf4920719f1629f9c6582
TKadyear <tamarakadyear@gmail.com>
2022-02-02 20:31:22 +0100

    Added directory for Mock the process of the fetch with testing

---
+++ src/test/mocks/response-correct.js
@@ -0,0 +1,83 @@
+const responseCorrect = [
+  {
+    "nameEvent": "evento 4, todo el año",
+    "free": true,
+    "price": 10.0,
+    "site": "Dirección.",
+    "cityLocation": "Ciudad",
+    "village": true,
+    "hoursOpen": "21:00",
+    "hoursClose": "23:30",
+    "dateStart": "2022/01/01",
+    "dateFinal": "2022/12/31",
+    "comments": "comentarios",
+    "category": [
+      "Music"
+    ],
+    "photoEvent": "link",
+    "linkTickets": "link",
+    "id": "evento-4-todo-el-ao",
+    "bookmark": false
+  },
+  {
+    "nameEvent": "evento 2",
+    "free": true,
+    "price": 10.0,
+    "site": "Dirección.",
+    "cityLocation": "Ciudad",
+    "village": true,
+    "hoursOpen": "21:00",
+    "hoursClose": "23:30",
+    "dateStart": "2021/02/06",
+    "dateFinal": "2022/12/06",
+    "comments": "comentarios",
+    "category": [
+      "Music"
+    ],
+    "photoEvent": "link",
+    "linkTickets": "link",
+    "id": "evento-2",
+    "bookmark": false
+  },
+  {
+    "nameEvent": "evento 1",
+    "free": false,
+    "price": 18.0,
+    "site": "Dirección.",
+    "cityLocation": "Ciudad",
+    "village": false,
+    "hoursOpen": "21:00",
+    "hoursClose": "23:30",
+    "dateStart": "2022/02/12",
+    "comments": "comentarios",
+    "category": [
+      "Music"
+    ],
+    "photoEvent": "link",
+    "atributionsPhoto": "algo",
+    "linkTickets": "link",
+    "id": "evento-1",
+    "bookmark": false
+  },
+  {
+    "nameEvent": "evento 3",
+    "free": true,
+    "price": 10.0,
+    "site": "Dirección.",
+    "cityLocation": "Ciudad",
+    "village": true,
+    "hoursOpen": "21:00",
+    "hoursClose": "23:30",
+    "dateStart": "2022/03/10",
+    "dateFinal": "2022/03/12",
+    "comments": "comentarios",
+    "category": [
+      "Music"
+    ],
+    "photoEvent": "link",
+    "linkTickets": "link",
+    "id": "evento-3",
+    "bookmark": false
+  },
+
+]
