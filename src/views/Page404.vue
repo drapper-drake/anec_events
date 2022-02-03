5d661542c29864ba7a4df51328e06d837e3e8f01
Gema Martin <geminway@live.com>
2022-02-03 14:17:36 +0100

    Fixed on page404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,16 +1,14 @@
 <template>
   <main class="mb-5 flex flex-col items-center md:mb-0">
-    <div
-      class="page-404 flex flex-col justify-center items-center not-found relative w-11/12 mx-auto pb-6 lg:m-0 lg:px-14 md:w-2/5"
-    >
+    <div class="page-404 flex flex-col items-center not-found relative w-full">
       <p class="info-notFound text-center">Esa ruta no existe</p>
       <img
-        class
+        class="w-[400px]"
         src="/img/404NotFound/404-notFound.svg"
         alt="404 No hemos encontrado la página que buscabas"
       />
       <img
-        class="max-h-44 bottom-0 right-10 absolute"
+        class="max-h-44 bottom-0 w-min right-90 absolute"
         src="/img/404NotFound/Pato-404-zapatos.svg"
         alt
       />
