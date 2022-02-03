564e37802abc0b18a3263d6818944436b24de8df
Gema Martin <geminway@live.com>
2022-02-03 11:59:28 +0100

    Added router before check robots.txt

---
+++ src/views/GoRobots.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'robots',
+  methods: {
+    goRobots() {
+      this.$router.push({ path: '/robots.txt' });
+    }
+  },
+  created() {
+    this.goRobots();
+  },
+}
+</script>
