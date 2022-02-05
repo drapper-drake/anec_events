c08c2831a03608119e0c42ffc2de964e358595a6
David Alvarez <marava.jr@gmail.com>
2022-02-05 15:16:52 +0100

    Add checkbox Need to unselect when category changes

---
+++ src/components/AppFilterFree.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full flex flex-row justify-center items-center my-4">
+    <input id="check" type="checkbox" class="w-3 h-3" v-model="free" />
+    <label
+      for="check"
+      class="m-1 w-44 font-sans font-bold text-sm text-bg-light"
+    >Solo eventos GRATUITOS</label>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      free: false
+    }
+  },
+  methods: {
+    filterFree() {
+      let listFilterFree = [];
+      if (this.free) {
+        listFilterFree = this.$store.state.currentListEvents.filter(event => event.free);
+        this.$store.dispatch('showFilteredEvents', listFilterFree);
+        this.$store.dispatch('divideList', 1);
+      }
+    }
+  },
+  watch: {
+    free() {
+      this.filterFree();
+    }
+  }
+}
+// ! Intentar que se deseleccione al cambiar de categoría
+</script>
