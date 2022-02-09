96412802e28df242d45f9685315353f09a3bd562
Gema Martín <geminway@live.com>
2022-02-09 20:27:31 +0100

    Fixed horizontal scroll bug on mobile

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -59,7 +59,7 @@ export default {
 </script>
 
 <template>
-    <div v-if="this.$store.state.showPagination" class="flex flex-row justify-center gap-2 p-6">
+    <div v-if="this.$store.state.showPagination" class="w-full flex flex-row justify-center gap-2 p-6">
         <ChevronLeft class="page-unselected h-[42px]" @click="changePage(1)" />
         <div class="container-pagination w-1/4 md:w-5/12 flex overflow-x-scroll gap-1">
             <button
