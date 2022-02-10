cd9cc4d03609b94a612c2ee4a03d012d0b7c055e
TKadyear <tamarakadyear@gmail.com>
2022-02-10 10:16:57 +0100

    Accesibility Footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,7 @@
       class="flex flex-col justify-between items-center gap-6 md:flex-row md:justify-around md:items-baseline md:gap-0"
     >
       <div class="w-48 flex flex-col gap-4">
-        <p>Síguenos</p>
+        <p tabindex="0">Síguenos</p>
         <div class="flex justify-around">
           <a href="https://twitter.com/anecevents" target="_blank" rel="noopener">
             <img class="w-8 h-8" src="/img/icons/twitter.svg" alt="Twitter" />
@@ -24,20 +24,18 @@
         </div>
       </div>
       <div class="flex flex-col gap-4">
-        <p>Contáctanos</p>
+        <p tabindex="0">Contáctanos</p>
         <a href="mailto:eoidapperdrake@gmail.com">eoidapperdrake@gmail.com</a>
       </div>
       <div class="flex flex-col gap-4">
-        <!-- <a href="./privacy.html">Aviso Legal</a> -->
         <router-link to="/privacy">Aviso Legal</router-link>
-        <!-- <a href="./cookies/cookies.html">Política de cookies</a> -->
         <router-link to="/cookies">Política de Cookies</router-link>
       </div>
     </div>
     <div class="flex flex-wrap justify-center items-center mb-2 gap-2">
       <img class="w-4 h-4" src="/img/Favicon.svg" alt="Logo" />
-      <p>Ànec Events.</p>
-      <p>Desarrollado por Dapper Drake</p>
+
+      <p tabindex="0">Ànec Events.Desarrollado por Dapper Drake</p>
     </div>
   </footer>
 </template>
